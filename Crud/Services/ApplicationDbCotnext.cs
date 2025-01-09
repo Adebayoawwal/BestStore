b1904f60045d7512db1744073bce5750dca6a063
Adebayo Awwal <adebayoawwaldele@gmail.com>
2025-01-09 18:07:30 +0100

    project comlete

---
+++ Crud/Services/ApplicationDbCotnext.cs
@@ -0,0 +1,13 @@
+﻿using Crud.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Crud.Services
+{
+    public class ApplicationDbCotnext : DbContext
+    {
+        public ApplicationDbCotnext(DbContextOptions options) :base(options) 
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
